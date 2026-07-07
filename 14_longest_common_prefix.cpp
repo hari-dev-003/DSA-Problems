c5eea4119b384645b2bb57ca5056559f2b0ef747
Hari Dev M V <hariharidev5@gmail.com>
2026-07-07 18:47:15 +0530

    Add 14_longest_common_prefix.cpp - LeetCode Problem 14

---
+++ 14_longest_common_prefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+        sort(strs.begin(),strs.end());
+        
+        string first = strs[0];
+        string last = strs[strs.size()-1];
+        int minLen = min(first.size(),last.size());
+        string ans="";
+        for(int i=0;i<minLen;i++){
+            if(first[i]!=last[i]) break;
+            ans+=first[i];
+        }
+        return ans;
+    }
+};
