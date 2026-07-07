8e0d71aa75e95ec3617c999b2a23ab4229abd572
Hari Dev M V <hariharidev5@gmail.com>
2026-07-07 19:48:53 +0530

    Add 189_rotate_array.cpp - LeetCode Problem 189

---
+++ 189_rotate_array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<int>& arr, int k) {
+        int n = arr.size();
+        k%=n;
+        reverse(arr.begin(),arr.end()-k);
+        reverse(arr.end()-k,arr.end());
+        reverse(arr.begin(),arr.end());
+
+    }
+};
